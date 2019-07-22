02f53846603cf76a242dab9f5ca3372d1f731b3b
Juan <jponce@mstech.la>
2019-07-22 17:35:14 -0300

    22.07

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angularTincho'),
+      dir: require('path').join(__dirname, './coverage/Todo'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { NgModule, Injectable } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { HttpClientModule } from '@angular/common/http';
+import { GestureConfig } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import 'hammerjs';
+
+declare var Hammer: any;
+@Injectable()
+export class HammerConfig extends GestureConfig {
+  buildHammer(element: HTMLElement) {
+    return new GestureConfig({ touchAction: 'pan-y' }).buildHammer(element);
+  }
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +25,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CoreModule,
+    HttpClientModule,
+    MatSnackBarModule
   ],
-  providers: [],
+  entryComponents: [
+
+  ],
+  providers: [{
+    provide: HAMMER_GESTURE_CONFIG,
+    useClass: HammerConfig
+  },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
